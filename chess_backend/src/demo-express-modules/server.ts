77edb7f6881a2666cae4f2ba7a6c084391961dac
alexis.pelissier <alexis.pelissier@edu.itescia.fr>
2023-04-18 19:31:29 +0200

    add link to backend, and base error handling, have to redirect

--- chess_backend/src/demo-express-modules/server.ts
+++ chess_backend/src/demo-express-modules/server.ts
@@ -7,6 +7,9 @@ export function initServer() {
 
   /* Middleware, console.log each request */
   app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     console.log(req.method, req.path, +new Date())
     next()
   })
