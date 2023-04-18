4fee173712edfe4fbf9e0af062ff5c45e8c17570
angel_rafael <generalpompeyo@hotmail.com>
2023-04-18 16:55:29 +0200

    added dashboard

---
+++ chess_frontend/src/app/pages/dashboard/Dashboard.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+test('renders Dashboard page', () => {
+  render(<Dashboard />);
+});
---
+++ chess_frontend/src/app/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,12 @@
+import Box from '@mui/material/Box';
+import "./Dashboard.css";
+
+function Dashboard() {
+  return (
+    <Box>
+      hello
+    </Box>
+  );
+}
+
+export default Dashboard;
