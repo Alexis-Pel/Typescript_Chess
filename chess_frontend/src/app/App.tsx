8c722f173f3912889a4105760ebb6b46de7fc00b
angel_rafael <generalpompeyo@hotmail.com>
2023-04-18 16:50:41 +0200

    Added Dashboard page, added mui dependency

--- chess_frontend/src/app/App.tsx
+++ chess_frontend/src/app/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
-import './App.css';
 import Login from './pages/login/Login';
 import Game from './pages/game/Game';
 import Lobby from './pages/lobby/Lobby';
+import Dashboard from './pages/dashboard/Dashboard';
+import './App.css';
 
 function App() {
   return (
     <Routes>
-      <Route path='/login' element={<Login />}/>
+      <Route path='/' element={<Login />}/>
+      <Route path='/dashboard' element={<Dashboard />} />
       <Route path='/game' element={<Game />}/>
       <Route path='/Lobby' element={<Lobby />}/>
     </Routes>
