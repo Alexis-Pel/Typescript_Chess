1810913d493c77f049583c5eb4585bf5bb737a11
alexis.pelissier <alexis.pelissier@edu.itescia.fr>
2023-04-18 18:43:11 +0200

    add base login form

--- chess_frontend/src/app/pages/login/Login.tsx
+++ chess_frontend/src/app/pages/login/Login.tsx
@@ -1,7 +1,24 @@
 import "./Login.css";
+import React from "react";
+import {TextField, Button} from "@mui/material";
 
 function Login() {
-  return <div>Login page</div>;
+  return (
+      <div className="main-wrapper">
+          <div className="login-wrapper">
+              <form className="form-wrapper">
+                  <div className="inputs">
+                      <TextField id="outlined-basic" label="Email" variant="outlined" size="small" style={{marginBottom: "1em"}} />
+                      <TextField id="outlined-basic" label="Password" variant="outlined" size="small" />
+                  </div>
+                  <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                      <Button variant="outlined">Se connecter</Button>
+                      <i style={{fontSize: "10px"}}>Besoin de <a href="#">créer un compte ?</a></i>
+                  </div>
+              </form>
+          </div>
+      </div>
+  );
 }
 
 export default Login;
