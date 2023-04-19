7a4cef1f209de33d5869b03109fd8bd84f238ad1
angel_rafael <generalpompeyo@hotmail.com>
2023-04-19 10:30:53 +0200

    Added buttons to dashboard

--- chess_frontend/src/app/services/register-service.tsx
+++ chess_frontend/src/app/services/register-service.tsx
@@ -1,29 +1,30 @@
 // Api calls go here
-import axios from "axios";
+import axios from 'axios';
 
 export function register(username: string, password: string, email: string) {
-    //send api request
-    const data = {
-        username: username,
-        password: password,
-        email: email
-    };
-    return runRegister(data)
+  //send api request
+  const data = {
+    username: username,
+    password: password,
+    email: email,
+  };
+  return runRegister(data);
 }
 
-async function runRegister(data: {}){
-    let toReturn = {}
-    await axios.post('http://localhost:8080/user/register', data, {
-        headers: {
-            'Content-Type': 'application/json'
-        }
+async function runRegister(data: object) {
+  let toReturn = {};
+  await axios
+    .post('http://localhost:8080/user/register', data, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
     })
-        .then((response) => {
-            toReturn = response.data
-        })
-        .catch((error) => {
-            toReturn = false
-        });
-    console.clear()
-    return toReturn
+    .then((response) => {
+      toReturn = response.data;
+    })
+    .catch((error) => {
+      toReturn = false;
+    });
+  console.clear();
+  return toReturn;
 }
