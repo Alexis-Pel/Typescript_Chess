cb33ebbf84fd54dfb55c3de14043599e43ff275d
alexis.pelissier <alexis.pelissier@edu.itescia.fr>
2023-04-19 11:17:42 +0200

    add dialog

--- chess_frontend/src/app/pages/login/Login.tsx
+++ chess_frontend/src/app/pages/login/Login.tsx
@@ -1,17 +1,42 @@
 import './Login.css';
 import React, { useState } from 'react';
-import { TextField, Button } from '@mui/material';
+import {
+  TextField,
+  Button,
+  Alert,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@mui/material';
 import { logIn } from '../../services/login-service';
 
 function Login() {
   const [usernameValue, setUsernameValue] = useState('');
   const [passwordValue, setPasswordValue] = useState('');
+  const [emailRecoverValue, setemailRecoverValue] = useState('');
+  const [open, setOpen] = React.useState(false);
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  function handleClose() {
+    setOpen(false);
+  }
+  function handleCloseSend() {
+    setOpen(false);
+    // Send Email
+  }
   function handleUsernameChange(event: any) {
     setUsernameValue(event.target.value);
   }
   function handlePasswordChange(event: any) {
     setPasswordValue(event.target.value);
+  }
+  function handleEmailRecoverChange(event: any) {
+    setemailRecoverValue(event.target.value);
   }
 
   function handleSubmit() {
@@ -49,11 +74,41 @@ function Login() {
               onChange={handlePasswordChange}
               type={'password'}
             />
+            <i style={{ fontSize: '10px' }}>
+              <a href={'#'} onClick={handleClickOpen}>
+                Mot de passe oublié ?
+              </a>
+            </i>
+            <Dialog open={open} onClose={handleClose}>
+              <DialogTitle>Reintitialisation du mot de passe</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Veuillez indiquer votre adresse e-mail pour recevoir un mail de changement de mot
+                  de passe
+                </DialogContentText>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="name"
+                  label="Email Address"
+                  type="email"
+                  fullWidth
+                  variant="standard"
+                  value={emailRecoverValue}
+                  onChange={handleEmailRecoverChange}
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handleCloseSend}>Send</Button>
+              </DialogActions>
+            </Dialog>
           </div>
           <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <Button variant="outlined" onClick={handleSubmit}>
               Se connecter
             </Button>
+
             <i style={{ fontSize: '10px' }}>
               Besoin de <a href="/register">créer un compte ?</a>
             </i>
