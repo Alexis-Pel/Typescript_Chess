d4cb3d703ff4cbebfab96b3b2fc8feafc852d80c
alexis.pelissier <alexis.pelissier@edu.itescia.fr>
2023-04-19 15:16:06 +0200

    sdf

--- chess_backend/src/demo-express-modules/server.ts
+++ chess_backend/src/demo-express-modules/server.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import { router as userRouter } from './user/user.routes'
 import { router as matchRouter } from './match/match.routes'
+// @ts-ignore
+//import { checkToken } from "./token";
 
 export function initServer() {
   const app = express()
@@ -11,6 +13,18 @@ export function initServer() {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+/*    const bearer = req.header('Authorization')
+    // @ts-ignore
+    if(req.path != '/user/login' && req.path != '/user/register' && req.path != '/'){
+      if(bearer == undefined){
+        res.redirect('/')
+      }
+      else{
+        if(!checkToken(bearer)){
+          res.redirect('/')
+        }
+      }
+    }*/
     console.log(req.method, req.path, +new Date())
     next()
   })
