9d0ec1f1f56a8bd6fde93127a47d772f4d74bbfc
B3r4ti0n <arcoucou77@gmail.com>
2023-04-20 15:46:13 +0200

    chessboard configuration basique

--- chess_frontend/src/app/pages/game/Game.tsx
+++ chess_frontend/src/app/pages/game/Game.tsx
@@ -2,15 +2,27 @@ import './Game.css';
 import { Chessboard } from 'react-chessboard';
 import { useState } from 'react';
 import { Square } from 'react-chessboard/dist/chessboard/types';
+import axios from 'axios';
 
 function Game() {
-  const [game, setGame] = useState('');
+  const [game, setGame] = useState();
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post('http://localhost:8080/games');
+      console.log(response.data); // Afficher la réponse du serveur
+      setGame(response.data.fen);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   function onDrop(sourceSquare: Square, targetSquare: Square) {
     console.log(sourceSquare);
     console.log(targetSquare);
     return true;
   }
+
   return (
     <div>
       <Chessboard
