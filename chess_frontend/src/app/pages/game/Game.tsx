b4b0b05eab99a0d8110ae7469e3cdfebab8e6122
B3r4ti0n <arcoucou77@gmail.com>
2023-04-20 15:00:36 +0200

    chessboard base

--- chess_frontend/src/app/pages/game/Game.tsx
+++ chess_frontend/src/app/pages/game/Game.tsx
@@ -1,7 +1,29 @@
 import './Game.css';
+import { Chessboard } from 'react-chessboard';
+import { useState } from 'react';
+import { Square } from 'react-chessboard/dist/chessboard/types';
 
 function Game() {
-  return <div>Game page</div>;
+  const [game, setGame] = useState('');
+
+  function onDrop(sourceSquare: Square, targetSquare: Square) {
+    console.log(sourceSquare);
+    console.log(targetSquare);
+    return true;
+  }
+  return (
+    <div>
+      <Chessboard
+        id="StyledBoard"
+        boardOrientation="black"
+        boardWidth={400}
+        position={game}
+        onPieceDrop={onDrop}
+        customDarkSquareStyle={{ backgroundColor: '#779952' }}
+        customLightSquareStyle={{ backgroundColor: '#edeed1' }}
+      />
+    </div>
+  );
 }
 
 export default Game;
